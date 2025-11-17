66eb3ee9e1b9d2bbba43f6212570dbd74f812695
chiksa14 <muhtabalievdamir@gmail.com>
2025-11-18 00:00:53 +0400

    fixed-code

--- homework-6.js
+++ homework-6.js
@@ -14,7 +14,7 @@ const user = {
 console.log(user)
 
 //2 example(созать объект, который будет хранить данные об автомобиле)
-const infoCar = {
+const carInfo = {
   brand: "Mersedes",
   model: "cls",
   releaseYear: 2022,
@@ -22,57 +22,83 @@ const infoCar = {
   boxType: "auto"
 }
 
-infoCar.carOwner = [user.name]
+carInfo.Owner = {user}
 
-console.log(infoCar)
+console.log(carInfo)
 
 //3 example(Написать функцию, которая аргументом будет принимать объект, описанный в пункте №4)
-function checkAndSetMaxSpeed(obj, maxSpeed) {
+function setMaximumSpeed(obj, maxSpeed) {
   if (!obj.hasOwnProperty('maxSpeed')) {
-    obj['maxSpeed'] = maxSpeed;
+    obj.maxSpeed = maxSpeed;
   }
 }
 
-checkAndSetMaxSpeed(infoCar, 280)
+setMaximumSpeed(carInfo, 280)
 
-console.log(infoCar)
+console.log(carInfo)
 
 //4 example(функцию, которая получает первым аргументом  — объект, а вторым аргументом — свойство объекта, которое нужно вывести и выводит его значение)
 function getPropertyValue(obj, property) {
   console.log(obj[property])
 }
 
-getPropertyValue(infoCar, "releaseYear")
+getPropertyValue(carInfo, "releaseYear")
 
 //5 example(Создать массив, который содержит названия продуктов (просто строки))
 let = ['хлеб', 'яйца', 'майонез', 'кетчуп']
 
 //6 example(Создать массив, состоящий из объектов, где объект представляет собой книгу )
 let frames = [
-  {name: "Александр", position: "Frontend Developer", experience: 2},
-  {name: "Евгений", position: "Backend Developer", experience: 5},
-  {name: "Кирилл", position: "HR-менеджер", experience: 1},
-  {name: "Денис", position: "Projet Manager", experience: 3}
+  {
+    name: "Александр",
+    position: "Frontend Developer",
+    experience: 2},
+  {
+    name: "Евгений", 
+    position: "Backend Developer", 
+    experience: 5},
+  {
+    name: "Кирилл", 
+    position: "HR-менеджер", 
+    experience: 1},
+  {
+    name: "Денис", 
+    position: "Projet Manager",
+    experience: 3}
 ]
 
-frames.push({name: "Артем", position:"TechLead", experience: 8})
+frames.push({
+  name: "Артем", 
+  position:"TechLead",
+  experience: 8})
 
 console.log(frames)
 
 //7 example(массив, состоящих из тех же книг, но относящийся к определенной вселенной )
-let teamExpertiseDomains = [
-  {direction: "Frontend", pl: "html,css,js"},
-  {direction: "Backend", pl: "python,JavaScript,Java"}
+let framesNew = [
+  {
+    name: "Матвей",
+    position: "Team Lead ",
+    experience: 7},
+  {
+    name: "Егор",
+    position: "Тестировщик",
+    experience: 5},
+  {
+    name: "Евгений",
+    position: "SMM-специалист",
+    experience: 3
+  }
 ]
 
-let teamComposition  = [...frames, ...teamExpertiseDomains]
+let teamComposition  = [...frames, ...framesNew]
 
 console.log(teamComposition)
 
 //8 example(map)
 const checkingWorkExperience = (framesArray) => 
-  framesArray.map(frames => ({ ...frames, isBeginer: frames.experience > 3}));
+  framesArray.map(teamComposition => ({ ...teamComposition, isBeginer: teamComposition.experience > 3}));
 
-const resultCheckingWorkExperince = checkingWorkExperience(frames)
+const resultCheckingWorkExperince = checkingWorkExperience(teamComposition)
 
 console.log(resultCheckingWorkExperince)
